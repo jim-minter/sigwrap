c0113846ae51433730981d5a32e499550f00b6f6
Jim Minter <jminter@redhat.com>
2015-01-26 16:13:18 +0000

    return exit code from child

--- sigwrap.c
+++ sigwrap.c
@@ -24,9 +24,18 @@ main(int argc, char *argv[]) {
 
   signal(SIGTERM, sigterm);
 
-  if((pid = fork()))
-    wait(NULL);
-  else
+  if((pid = fork())) {
+    int status;
+    wait(&status);
+
+    signal(SIGTERM, SIG_DFL);
+
+    if(WIFEXITED(status))
+      return WEXITSTATUS(status);
+    else if(WIFSIGNALED(status))
+      kill(getpid(), WTERMSIG(status));
+
+  } else
     execve(argv[1], argv + 1, environ);
 
   return 0;
